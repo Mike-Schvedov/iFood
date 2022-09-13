c2f552556ca7abb8801771dc450555294f2d2317
Mike Schvedov <you@example.com>
2022-09-13 20:17:38 +0300

    Some cleaning of the xml

--- app/src/main/java/com/mikeschvedov/ifood/construction/TodoList.kt
+++ app/src/main/java/com/mikeschvedov/ifood/construction/TodoList.kt
@@ -3,3 +3,9 @@ package com.mikeschvedov.ifood.construction
 //TODO Leave the weight fragment and chart to the end, it is a feature that is optional in the future
 // See the todo to combine both unit/gram edittexts
 //TODO add app widget
+
+
+
+// ------ bug fixes -----
+// TODO when we click on the x and remove the query,
+//  we are stuck with the unit/grams layout insead of returning to the list
